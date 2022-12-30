f6813259ac5c9c6c384889c227cabfbb2d54ad37
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-12-30 20:50:58 -0300

    Create entities, exceptions and exceptionHandler

---
+++ src/main/java/com/devsuperior/movieflix/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.devsuperior.movieflix.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.movieflix.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+}
